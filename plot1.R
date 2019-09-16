172f35184ef95ad2cd548fd6a7c1820bf872f003
KMeraner <katharina.meraner@web.de>
2019-09-16 22:15:16 +0200

    Make Plot 1

---
+++ plot1.R
@@ -0,0 +1,25 @@
+# Coursera Course: Exploratory Analysis
+# Assignment 1 - Plot 1
+# Date: 16.09.2019
+
+# Download the Individual household electric power consumption data set
+library(sqldf)
+ifile <- 'power_consumption.zip'
+if (!(file.exists(ifile))){
+    url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+    download.file(url,destfile=ifile,method='curl')
+    unzip(ifile)
+}
+
+# Read subset of data 
+sql_query = "SELECT * FROM file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+if (!exists('hpc')) {hpc<-read.csv2.sql("household_power_consumption.txt", sql = sql_query,na.Strings='?')
+    # Combine Date and Time to one 'Date-column and remove the Time column
+    hpc$Date <- strptime(paste(hpc$Date,hpc$Time),format='%d/%m/%Y %H:%M:%S')
+    hpc$Time <- NULL}
+
+# Make plot 1
+png(file='plot1.png',width = 480, height = 480, unit = "px")
+par(mfrow=c(1,1))
+hist(hpc$Global_active_power,col='red',xlab='Global Active Power (kilowatts)',main='Global Active Power')
+dev.off()
